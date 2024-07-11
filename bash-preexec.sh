ae1130afe9f2056b42de2fcea9d3116a4d342de5
Koichi Murase <myoga.murase@gmail.com>
2024-07-12 01:45:08 +0900

    Use BATS_VERSION

--- bash-preexec.sh
+++ bash-preexec.sh
@@ -47,6 +47,12 @@ if [[ -z "${BASH_VERSINFO-}" ]] || (( BASH_VERSINFO[0] < 3 || (BASH_VERSINFO[0] 
     return 1
 fi
 
+# We do not enable bash-preexc in non-interactive shells (except in tests).
+# `BATS_VERSION` is set by "bats" for "tests/*.bats".
+if [[ $- != *i* && ! ${BATS_VERSION-} ]]; then
+    return 0
+fi
+
 # Avoid duplicate inclusion
 if [[ -n "${bash_preexec_imported:-}" || -n "${__bp_imported:-}" ]]; then
     return 0
